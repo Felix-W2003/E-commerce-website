c6f338a4381abbf0dbc689ecdd8988453cdcd471
Felix-W2003 <rey_relia@qq.com>
2026-08-22 12:34:02 +0800

    layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    <div>im layout view</div>
-
-    <RouterView />
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
-
-<script setup lang="ts"></script>
