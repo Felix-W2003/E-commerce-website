6878866d309e9bf1b2cfd0417f15dec2216bb0a9
Felix-W2003 <rey_relia@qq.com>
2026-08-22 12:23:17 +0800

    静态资源的创建

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import {getCategory} from '@/apis/testApi.ts'
 import router from '@/router/index.ts'
+import '@/styles/common.scss'
 const app = createApp(App)
 // getCategory().then(res =>{
 //     console.log(res)
