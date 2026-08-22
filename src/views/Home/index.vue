5053783735982daec21fa6281eb300ef82d01410
Felix-W2003 <rey_relia@qq.com>
2026-08-22 17:06:56 +0800

    组件复用

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,14 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+
+  <!-- 测试面板组件 -->
+   <HomePanel title="新鲜好物" subTitle="新鲜好物，好多商品">
+    <div>hello world</div>
+    </HomePanel>
+
+    <HomePanel title="人气推荐" subTitle="人气推荐，好多推荐">
+    <div>hello vue3</div>
+    </HomePanel>
 </template>
