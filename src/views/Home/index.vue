1c022b420d0a4162e56ff6bac40966cc937aa37d
Felix-W2003 <rey_relia@qq.com>
2026-08-22 13:11:48 +0800

    吸顶导航实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>im Home view</div>
+    <div style="height: 1000px;width: 1020px;background-color: gray; margin: 0 auto;">im Home view</div>
 </template>
 
 <script setup lang="ts"></script>
