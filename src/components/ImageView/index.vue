37c7a5cdf93221722bac145e78f86f20165f8c9a
Felix-W2003 <rey_relia@qq.com>
2026-08-23 18:54:00 +0800

    图片放大镜

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -14,14 +14,18 @@ const activeIndex = ref(0)
 
 const enterhandler = (i)=>{
     activeIndex.value = i
-}
+} 
 
 const target = ref(null)
 const {elementX,elementY,isOutside} = useMouseInElement(target)
 const left = ref(0)
 const top = ref(0)
-watch([elementX,elementY],()=>{
+const positionX = ref(0)
+const positionY = ref(0)
+watch([elementX,elementY,isOutside],()=>{
   //有效范围内控制滑块
+ if (isOutside.value===false){
+  console.log('jisuan')
   //横向
   if (elementX.value>100&&elementX.value<300){
     left.value = elementX.value-100
@@ -41,12 +45,19 @@ watch([elementX,elementY],()=>{
   
    if(elementY.value>300){
     top.value=200
+    
   }
   if(elementY.value<100){
     top.value=0
   }
   
+  positionX.value = -left.value*2
+  positionY.value = -top.value*2
+}
 })
+
+
+
 </script>
 
 
@@ -57,7 +68,7 @@ watch([elementX,elementY],()=>{
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -68,11 +79,11 @@ watch([elementX,elementY],()=>{
     <!-- 放大镜大图 -->
     <div class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundImage: `url(${imageList[activeIndex]})`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside" ></div>
   </div>
 </template>
 
