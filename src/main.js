ad1b97343630ad9f83f215c0105a4d7da8dacf25
Felix-W2003 <rey_relia@qq.com>
2026-08-23 21:06:36 +0800

    全局components组件注册复用

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import router from '@/router/index.js'
 import '@/styles/common.scss'
 // 定义懒加载插件
 import {lazyPlugin} from '@/directives'
+import {componentPlugin} from '@/components'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(lazyPlugin)
+app.use(componentPlugin)
 app.mount('#app')
