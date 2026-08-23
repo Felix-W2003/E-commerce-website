bc4771c5b738a6595f41fb70d9d8fcf81ecf87c9
Felix-W2003 <rey_relia@qq.com>
2026-08-23 13:49:03 +0800

    路由滚动行为配置

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,13 @@ const router = createRouter({
       path: '/login',
       component: Login
     }
-  ]
+  ],
+  //路由滚动行为配置
+  scrollBehavior(){
+    return {
+      top:0
+    }
+  }
 })
 
 export default router
