8c56a7b961b18451ccd5ac0733c859b0be834c8b
Felix-W2003 <rey_relia@qq.com>
2026-08-24 14:30:38 +0800

    处理401错误

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import{useUserStore} from '@/stores/user.js'
+import  router  from '@/router'
 // 创建axios实例
 const httpInstance = axios.create({
   baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net/',
@@ -22,10 +23,21 @@ httpInstance.interceptors.request.use(config => {
 // axios响应式拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
   //统一错误处理
+  const userStore = useUserStore()
   ElMessage({
     type:'warning',
     message:e.response.data.message
   })
+
+
+  // 401 token失效处理
+  if(e.response.status === 401){
+  // 1. 清除本地用户数据
+ 
+  userStore.clearUserInfo()
+  router.push('/login')
+  // 2. 跳转到登录页
+  }
   return Promise.reject(e)
 })
 
